905ef07b1a4187dbdbf8b55b21474ae3a82f555d
sneha06 <snehaacharya06@gmail.com>
2015-01-26 22:32:13 +0530

    SignUp page dsign

---
+++ app/src/main/java/com/android/sneha/chatapp/GetUsernameTask.java
@@ -0,0 +1,7 @@
+package com.android.sneha.chatapp;
+
+/**
+ * Created by sneha on 20/1/15.
+ */
+public class GetUsernameTask {
+}
---
+++ app/src/main/java/com/android/sneha/chatapp/ParseApplication.java
@@ -0,0 +1,7 @@
+package com.android.sneha.chatapp;
+
+/**
+ * Created by sneha on 26/1/15.
+ */
+public class ParseApplication {
+}
